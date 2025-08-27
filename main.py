91f1cfbefbb050c7515f96f34000ebd6ed50217d
Rida Malik <internalprogression@gmail.com>
2025-08-27 19:28:29 +0500

    Basic ScreenRecorder that records screen for a specified amount of time, in this case 10 seconds.

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import pyautogui
+from win32api import GetSystemMetrics
+import numpy as np
+import time
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+dimension = (width, height)
+
+f = cv2.VideoWriter_fourcc(*"mp4v")
+
+output = cv2.VideoWriter("test.mp4", f, 30.0, dimension)
+
+start_time_now = time.time()
+
+duration = 10
+
+end_time = start_time_now + duration
+
+while True:
+    image = pyautogui.screenshot()
+    frame_1 = np.array(image)
+    frame = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
+
+    output.write(frame)
+    c_time = time.time()
+    if c_time > end_time:
+        break
+
+output.release()
+
+print("--- END ---")
